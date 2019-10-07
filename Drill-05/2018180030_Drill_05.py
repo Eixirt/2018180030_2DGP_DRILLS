833081fa52e674aa655afe9924a4ccce587ea25b
DogyGyuLee (Laptop) <ledogy333@gmail.com>
2019-10-07 18:45:35 +0900

    Drill05_02_DrawBackGround
    산기대 이미지 출력

--- Drill-05/2018180030_Drill_05.py
+++ Drill-05/2018180030_Drill_05.py
@@ -1,5 +1,18 @@
 import pico2d
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+pico2d.open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
-pico2d.open_canvas()
+background = pico2d.load_image('KPU_GROUND.png')
+mouse = pico2d.load_image('hand_arrow.png')
+character = pico2d.load_image('animation_sheet.png')
+
+running = True
+
+while running:
+    pico2d.clear_canvas()
+    background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+
+    pico2d.update_canvas()
+    pico2d.delay(0.1)
 
 pico2d.close_canvas()

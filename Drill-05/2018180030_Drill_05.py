37e637887ae7d6340bc986e956f7c2983a2b90a8
DogyGyuLee (Laptop) <ledogy333@gmail.com>
2019-10-07 18:57:38 +0900

    Drill05_04_MovingMouse
    마우스 움직임

--- Drill-05/2018180030_Drill_05.py
+++ Drill-05/2018180030_Drill_05.py
@@ -11,17 +11,34 @@ running = True
 frame_x = 0
 frame_y = 1
 
-x = 20
+mouse_x = 320
+mouse_y = 350
+
+
+def handle_events():
+    global running
+    global mouse_x, mouse_y
+
+    events = pico2d.get_events()
+    for event in events:
+        if event.type == pico2d.SDL_KEYDOWN and event.key == pico2d.SDLK_ESCAPE:
+            running = False
+        elif event.type == pico2d.SDL_MOUSEMOTION:
+            mouse_x, mouse_y = event.x, KPU_HEIGHT - 1 - event.y
+
+
+pico2d.hide_cursor()
 
 while running:
     pico2d.clear_canvas()
     background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
 
-    mouse.draw(350, 350)
-    character.clip_draw(frame_x * 100, frame_y * 100, 100, 100, x + 240, 290)
+    mouse.draw(mouse_x, mouse_y)
+    character.clip_draw(frame_x * 100, frame_y * 100, 100, 100, 100 + 240, 290)
     frame_x = (frame_x + 1) % 8
 
     pico2d.update_canvas()
     pico2d.delay(0.05)
+    handle_events()
 
 pico2d.close_canvas()

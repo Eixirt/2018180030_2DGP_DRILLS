91cf7ac78b6177432e860f8041b7d5840d8caaf3
DogyGyuLee (Laptop) <ledogy333@gmail.com>
2019-10-07 19:16:35 +0900

    Drill_05_06_ClickMove
    클릭 이동 구현

--- Drill-05/2018180030_Drill_05.py
+++ Drill-05/2018180030_Drill_05.py
@@ -18,12 +18,16 @@ player_y = 500
 
 goto_x = 500
 goto_y = 500
+cnt = 0
 
+is_moving = False
 
 def handle_events():
     global running
     global mouse_x, mouse_y
     global goto_x, goto_y
+    global is_moving
+    global cnt
 
     events = pico2d.get_events()
     for event in events:
@@ -33,6 +37,25 @@ def handle_events():
             mouse_x, mouse_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == pico2d.SDL_MOUSEBUTTONDOWN:
             goto_x, goto_y = event.x - 25, KPU_HEIGHT - 1 - event.y + 26
+            is_moving = True
+            cnt = 0
+
+
+def moving_character():
+    global goto_x, goto_y
+    global player_x, player_y
+    global is_moving
+    global frame_x, frame_y
+    global cnt
+
+    t = cnt / 100
+
+    if t < 1 and (player_x != goto_x or player_y != goto_y):
+        player_x = (1 - t) * player_x + t * goto_x
+        player_y = (1 - t) * player_y + t * goto_y
+        cnt += 2
+    else:
+        is_moving = False
 
 
 pico2d.hide_cursor()
@@ -41,12 +64,15 @@ while running:
     pico2d.clear_canvas()
     background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
 
-    character.clip_draw(frame_x * 100, frame_y * 100, 100, 100, goto_x, goto_y)
+    character.clip_draw(frame_x * 100, frame_y * 100, 100, 100, player_x, player_y)
     mouse.draw(mouse_x, mouse_y)
     frame_x = (frame_x + 1) % 8
 
     pico2d.update_canvas()
     pico2d.delay(0.05)
     handle_events()
+
+    if is_moving:
+        moving_character()
 
 pico2d.close_canvas()

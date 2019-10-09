27324147930a743a359d452858554239433ea721
DogyGyuLee (Laptop) <ledogy333@gmail.com>
2019-10-09 18:06:19 +0900

    Drill_06_02_Init2
    초기화2

--- Drill-06/2018180030_Drill_06_1.py
+++ Drill-06/2018180030_Drill_06_1.py
@@ -106,7 +106,7 @@ prepare_turtle_canvas()
 # draw_curve_3_points((-350, -100), (-50, 200), (150, -100))
 # draw_curve_3_points((-50, 200), (150, -100), (350, 300))
 
-draw_curve_4_points((-350, -100), (-50, 200), (150, -100), (350, 300))
+draw_curve_4_points((-300, 200), (400, 350), (300, -300), (-200, -200))
 
 
 turtle.done()

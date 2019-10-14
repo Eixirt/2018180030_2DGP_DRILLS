9140368a2eac3de3368c90fae1898b6b189e7712
DogyGyuLee (Laptop) <ledogy333@gmail.com>
2019-10-14 18:56:03 +0900

    Drill_07_02_완성
    Drill07 - 2018180030 - 이동규
    완료 커밋 횟수 : 2회
    https://youtu.be/uVDtplKNDo4

--- Drill-07/2018180030_Drill_07.py
+++ Drill-07/2018180030_Drill_07.py
@@ -1,6 +1,7 @@
 from pico2d import *
 from random import *
 
+
 # Game object class here
 class Grass:
     def __init__(self):
@@ -24,6 +25,29 @@ class Boy:
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 
+class Ball:
+    def __init__(self):
+        self.x, self.y = randint(100, 700), 599
+        self.size = randint(0, 1)  # 0 = 21x21 / 1 = 41x41
+        self.fall_speed = randint(5, 50)
+        if self.size == 0:
+            self.image = load_image('ball21x21.png')
+        else:
+            self.image = load_image('ball41x41.png')
+
+    def update(self):
+        if self.y > 30:
+            self.y = self.y - self.fall_speed
+            if self.y <= 30:
+                self.y = 30
+
+    def draw(self):
+        if self.size == 0:
+            self.image.draw(self.x, self.y + 35)
+        else:
+            self.image.draw(self.x, self.y + 45)
+
+
 def handle_events():
     global running
     events = get_events()
@@ -38,6 +62,8 @@ def handle_events():
 open_canvas()
 boy = Boy()
 grass = Grass()
+
+balls = [Ball() for i in range(20)]
 team = [Boy() for i in range(11)]
 
 running = True
@@ -50,11 +76,17 @@ while running:
     for boy in team:
         boy.update()
 
+    for ball in balls:
+        ball.update()
+
     clear_canvas()
 
     grass.draw()
     for boy in team:
         boy.draw()
+
+    for ball in balls:
+        ball.draw()
 
     update_canvas()
 

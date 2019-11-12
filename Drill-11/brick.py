b14d0cc3ff0a3c4d80d7f5231df0cc62875e2853
DogyGyuLee (Laptop) <ledogy333@gmail.com>
2019-11-13 01:38:27 +0900

    Drill_11_02_Moving_Brick
    벽돌 움직임 구현

---
+++ Drill-11/brick.py
@@ -0,0 +1,37 @@
+import random
+from pico2d import *
+import game_world
+import game_framework
+
+
+class Brick:
+    image = None
+
+    def __init__(self):
+        if Brick.image is None:
+            Brick.image = load_image('brick180x40.png')
+        self.x, self.y, self.velocity = random.randint(200, 1300-1), 150, 300
+        self.dir = 1
+
+    def get_bb(self):
+        # fill here
+        return self.x - Brick.image.w / 2, self.y - Brick.image.h / 2,\
+               self.x + Brick.image.w / 2, self.y + Brick.image.h / 2
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        # fill here for draw
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        self.x -= self.dir * self.velocity * game_framework.frame_time
+        if self.x > 1300 - 1 or self.x < 200:
+            self.dir = -self.dir
+
+    # fill here for def stop
+    def stop(self):
+        self.velocity = 0
+
+    pass
+
+
